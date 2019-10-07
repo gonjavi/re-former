99983c2f95179db51a0ef171b0c79824cbffd4be
gonjavi <javiercaliescali@gmail.com>
2019-10-07 16:36:06 -0500

    creating partial for the form, adding some bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, length:{minimum: 2}
-    validates :email,  presence: true
+    validates :email,  presence: true, uniqueness: true, email:true
     validates :password, presence: true, length:{within: 3..10}
 
 end
